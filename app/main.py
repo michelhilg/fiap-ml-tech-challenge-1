d4af2cfcd09305c7a8d00c967d30e561a500b593
Michel Hilgemberg <michelhilg@gmail.com>
2025-08-02 15:03:12 -0300

    Populate db with csv data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from . import models, routes
-from .database import engine
+from .database import engine, check_and_populate_db
 
 # Cria as tabelas no banco de dados
 models.Base.metadata.create_all(bind=engine)
@@ -11,5 +11,10 @@ app = FastAPI(
     version="0.1.0",
     description="Uma API para consulta de dados de livros extraídos via web scraping.",
 )
+
+# Populando o banco de dados na inicialização
+@app.on_event("startup")
+def on_startup():
+    check_and_populate_db() 
 
 app.include_router(routes.router)
