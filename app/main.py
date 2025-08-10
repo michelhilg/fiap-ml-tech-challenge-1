f9f9dd1ed8b653fc3ceeea4727945923971fc56a
Michel Hilgemberg <michelhilg@gmail.com>
2025-08-10 08:59:19 -0300

    Add config details to code

--- app/main.py
+++ app/main.py
@@ -2,15 +2,17 @@ from fastapi import FastAPI
 from . import models, routes
 from .database import engine, check_and_populate_db 
 from .ml import ml_routes 
+from .config import api_description, servers
 
 # Cria as tabelas no banco de dados
 models.Base.metadata.create_all(bind=engine)
 
 # Cria a instância principal da aplicação FastAPI
 app = FastAPI(
-    title="API de Consulta de Livros",
+    title="FIAP ML Tech Challenge 1 - API com Web Scraping",
     version="0.1.0",
-    description="Uma API para consulta de dados de livros extraídos via web scraping.",
+    description=api_description,
+    servers=servers,
 )
 
 # Popula o banco de dados na inicialização
